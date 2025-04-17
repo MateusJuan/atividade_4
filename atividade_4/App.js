bed84179d6e6682d1dc5c45160fd31b384f22dab
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2025-04-16 21:41:52 -0300

    16/04/2025

--- atividade_4/App.js
+++ atividade_4/App.js
@@ -1,7 +1,15 @@
 import { MaterialIcons, FontAwesome } from '@expo/vector-icons';
 import React from 'react';
-import {SafeAreaView,View,StyleSheet,Text,TextInput,TouchableOpacity, ScrollView, Image } from 'react-native';
-
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from 'react-native';
 import { Avatar } from 'react-native-elements';
 
 const categorias = [
@@ -14,11 +22,22 @@ const categorias = [
   { nome: 'Hostel', icone: 'bed', tipo: 'FontAwesome' },
   { nome: 'Veja mais ', icone: 'th-large', tipo: 'FontAwesome' }
 ];
-const imagens = [
-  'https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/nationalgeographic2710344.jpg?w=1600',
+
+const imagens_populares = [
+  'https://static.todamateria.com.br/upload/es/ta/estado-de-pernambuco-og.jpg?class=ogImageWide',
+  'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/0f/9a/fc/66.jpg',
   'https://www.vaidepromo.com.br/blog/wp-content/uploads/2024/03/Cidade-mais-visitada-dos-EUA-1024x682.jpg',
   'https://midias.eurodicas.com.br/wp-content/uploads/2024/07/tudo-sobre-espanha-1-824x420.jpg.webp',
-  'https://res.cloudinary.com/worldpackers/image/upload/c_fill,f_auto,q_auto,w_1024/v1/guides/article_cover/qu18o8metz5esnm01sdv?_a=BACADKGT'
+  'https://res.cloudinary.com/worldpackers/image/upload/c_fill,f_auto,q_auto,w_1024/v1/guides/article_cover/qu18o8metz5esnm01sdv?_a=BACADKGT',
+    'https://www.essemundoenosso.com.br/wp-content/uploads/2021/12/vila-mangueira-destaque.jpg',
+  'https://enoestilo.com.br/wp-content/uploads/2014/06/paris-france-revista-enoestilo.jpg'
+];
+
+const imagens_recomendadas = [
+  'https://static.todamateria.com.br/upload/es/ta/estado-de-pernambuco-og.jpg?class=ogImageWide',
+  'https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/0f/9a/fc/66.jpg',
+  'https://www.essemundoenosso.com.br/wp-content/uploads/2021/12/vila-mangueira-destaque.jpg',
+  'https://enoestilo.com.br/wp-content/uploads/2014/06/paris-france-revista-enoestilo.jpg'
 ];
 
 export default function App() {
@@ -62,50 +81,62 @@ export default function App() {
         </View>
       </View>
 
-      {/* CONTEÚDO PRINCIPAL */}
-      <View style={estilos.conteudo}>
-          <View style={estilos.linhaTitulo}>
-            <Text style={estilos.conteudo_principal}>Categoria</Text>
-            <TouchableOpacity>
-              <MaterialIcons name='menu' size={30} color='#665DD1' />
-            </TouchableOpacity>
-          </View>
-
-          <View style={estilos.grid}>
-            {categorias.map((item, index) => (
-              <TouchableOpacity key={index} style={estilos.itemCategoria}>
-                <View style={estilos.circuloIcone}>
-                  {item.tipo === 'MaterialIcons' ? (
-                    <MaterialIcons name={item.icone} size={28} color="#fff" />
-                  ) : (
-                    <FontAwesome name={item.icone} size={28} color="#fff" />
-                  )}
-                </View>
-                <Text style={estilos.textoCategoria}>{item.nome}</Text>
-              </TouchableOpacity>
-            ))}
-          </View>
-
-          <View>
-          <View style={estilos.linhaTitulo}>
-            <Text style={estilos.conteudo_principal}>Destinos Populares</Text>
-            <TouchableOpacity>
-              <MaterialIcons name='menu' size={30} color='#665DD1' />
-            </TouchableOpacity>
-          </View>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            {imagens.map((url, index) => (
-              <View key={index} style={estilos.cardDestino}>
-                <Image
-                  source={{ uri: url }}
-                  style={estilos.imagemDestino}
-                  resizeMode="cover"
-                />
-              </View>
-            ))}
-          </ScrollView>
+      {/* CONTEÚDO PRINCIPAL COM SCROLL */}
+      <ScrollView style={estilos.conteudo} showsVerticalScrollIndicator={false}>
+        <View style={estilos.linhaTitulo}>
+          <Text style={estilos.conteudo_principal}>Categoria</Text>
+          <TouchableOpacity>
+            <MaterialIcons name="menu" size={30} color="#665DD1" />
+          </TouchableOpacity>
         </View>
-      </View>
+
+        <View style={estilos.grid}>
+          {categorias.map((item, index) => (
+            <TouchableOpacity key={index} style={estilos.itemCategoria}>
+              <View style={estilos.circuloIcone}>
+                {item.tipo === 'MaterialIcons' ? (
+                  <MaterialIcons name={item.icone} size={28} color="#fff" />
+                ) : (
+                  <FontAwesome name={item.icone} size={28} color="#fff" />
+                )}
+              </View>
+              <Text style={estilos.textoCategoria}>{item.nome}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <View style={estilos.linhaTitulo}>
+          <Text style={estilos.conteudo_principal}>Destinos Populares</Text>
+          <TouchableOpacity>
+            <MaterialIcons name="menu" size={30} color="#665DD1" />
+          </TouchableOpacity>
+        </View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {imagens_populares.map((url, index) => (
+            <View key={index} style={estilos.cardDestino}>
+              <Image
+                source={{ uri: url }}
+                style={estilos.imagemDestino}
+                resizeMode="cover"
+              />
+            </View>
+          ))}
+        </ScrollView>
+
+        <View style={estilos.linhaTitulo}>
+          <Text style={estilos.conteudo_principal}>Recomendados</Text>
+        </View>
+        {imagens_recomendadas.map((url, index) => (
+          <View key={index} style={estilos.cardRecomendadoVertical}>
+            <Image
+              source={{ uri: url }}
+              style={estilos.imagemRecomendadoVertical}
+              resizeMode="cover"
+            />
+          </View>
+        ))}
+        <View style={{ height: 100 }} />
+      </ScrollView>
 
       {/* MENU FIXO INFERIOR */}
       <View style={estilos.menu}>
@@ -144,14 +175,10 @@ const estilos = StyleSheet.create({
     paddingTop: 50,
     borderBottomLeftRadius: 25,
     borderBottomRightRadius: 25,
-  
-    // Sombra para iOS
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
     shadowRadius: 4,
-  
-    // Sombra para Android
     elevation: 8,
   },
   caixaCabecalho: {
@@ -214,7 +241,7 @@ const estilos = StyleSheet.create({
   },
   conteudo_principal: {
     fontSize: 30,
-    color:'gray'
+    color: 'gray',
   },
   menu: {
     flexDirection: 'row',
@@ -270,8 +297,19 @@ const estilos = StyleSheet.create({
     borderRadius: 15,
     overflow: 'hidden',
   },
-  
   imagemDestino: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 15,
+  },
+  cardRecomendadoVertical: {
+    width: '100%',
+    height: 180,
+    marginBottom: 16,
+    borderRadius: 15,
+    overflow: 'hidden',
+  },
+  imagemRecomendadoVertical: {
     width: '100%',
     height: '100%',
     borderRadius: 15,
